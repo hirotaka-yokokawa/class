270ef299a93683f42d3ad9e2bd53b3ffeb68fa04
Hirotaka <yokokawa.491992@gmail.com>
2018-11-18 15:54:08 +0900

    クラス使うと凄く便利｡共通項作るならクラスで作ると良いかな｡

--- product.py
+++ product.py
@@ -1,7 +1,13 @@
 class Product:
-    def __init__(self, name, price):
+    def __init__(self, name, amount): # インスタンス変数
         self.name = name
-        self.price = price
+        self.amount = amount
+
+    def discount(self, price):
+        self.amount -= price
+
+    def innfulate(self, price):
+        self.amount += price
 
 
 # passは空っぽという意味
@@ -11,8 +17,12 @@ if __name__ == "__main__":
     p1 = Product("iPhone", 100000)  # プロダクトクラスのインスタンス化
     print(p1)
     print(p1.name)  # iPhone
-    print(p1.price)  # 100000
+    print(p1.amount)  # 100000
+    p1.discount(5000)
+    print(p1.amount)
 
     p2 = Product("MBA", 150000)
     print(p2.name)
-    print(p2.price)
+    print(p2.amount)
+    p2.discount(9500)
+    print(p2.amount)
