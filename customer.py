1d14148ede95bda844a341c1bf2e4344c9ad3e0f
Hirotaka <yokokawa.491992@gmail.com>
2018-11-18 17:29:23 +0900

    カウントアップ､ダウンはクラス内で行う｡

--- customer.py
+++ customer.py
@@ -5,20 +5,21 @@ class Customer:
         self.age = age
 
     def full_name(self):
-        return  self.first_name + self.family_name
+        # return self.first_name + self.family_name
+        return self.family_name + self.first_name
 
     def display_profile(self):
         print(f"Name: {self.full_name()}, Age: {self.age}")
 
+    def display_full_name(self):
+        print(self.full_name())
+
 
 if __name__ == "__main__":
     tom = Customer("Tom", "Ford", 57)
-    print(tom.full_name())
-    tom.display_profile()
-
     ken = Customer("Ken", "Yokoyama", 49)
-    print(ken.full_name())
+
+    tom.display_profile()  # "Name: TomFord, Age: 57" と出力する
     ken.display_profile()
 
-
-
+    ken.display_full_name()
