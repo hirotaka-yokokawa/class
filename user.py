5e0b183a1ef27574de4adb45b0e88993e39d4b95
Hirotaka <yokokawa.491992@gmail.com>
2018-11-18 14:53:57 +0900

    インスタンスメソッド / 関数と同じ感じ｡ 変えたら大元のものでくくる｡

--- user.py
+++ user.py
@@ -5,11 +5,26 @@ class User:
         self.age = age
         self.country = country
 
+    def display_profile(self):
+        # display_profile() は Userクラスの インスタンスメソッド
+        print(f"私は{self.name}です｡国籍は{self.country}の{self.age}歳です｡")
+
+    def change_nationality(self,country_name):
+        self.country = country_name
 
 
 if __name__ == "__main__":
-    Bob = User("Bob", 15, "America") # Userクラスをインスタンス化 インスタンス=事実
+    Bob = User("Bob", 15, "USA") # Userクラスをインスタンス化 インスタンス=事実
+    Bob.display_profile()
+    Bob.change_nationality("China")
+    Bob.display_profile()
 
-    Tom = User("Tom", 57, "canada")
+    Tom = User("Tom", 57, "USA")
+    Tom.display_profile()
+    Tom.change_nationality("UK")
+    Tom.display_profile()
 
-    Ken = User("Ken", 49, "Japan")
+    Ken = User("Ken", 49, "Jp")
+    Ken.display_profile()
+    Ken.change_nationality("In")
+    Ken.display_profile()
