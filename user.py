e056acc84bb1f695e751e17c88bebe38efbdb938
Hirotaka <yokokawa.491992@gmail.com>
2018-11-18 14:22:19 +0900

    インスタンス変数と呼ぶらしいよ

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 class User:
     def __init__(self, name, age, country):
+        # インスタンス変数
         self.name = name
         self.age = age
         self.country = country
@@ -9,17 +10,6 @@ class User:
 if __name__ == "__main__":
     Bob = User("Bob", 15, "America") # Userクラスをインスタンス化 インスタンス=事実
 
-    print(Bob) #<__main__.User object at 0x10cd1f390>
-    print(Bob.name) # Bob
-    print(Bob.age)
-    print(Bob.country)
-
     Tom = User("Tom", 57, "canada")
-    print(Tom)
-    print(Tom.name)
-    print(Tom.country)
 
     Ken = User("Ken", 49, "Japan")
-    print(Ken)
-    print(Ken.name)
-    print(Ken.country)
